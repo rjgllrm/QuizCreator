9a19340f5ea9377148bf2ee125b82a910ce96468
Erica Guillermo <ericajhaneg@gmail.com>
2025-05-07 13:15:09 +0800

    Update quiz_maker.py
    setting up the size and orientation of window for the result

--- quiz_maker.py
+++ quiz_maker.py
@@ -6,3 +6,9 @@ from PyQt5.QtWidgets import (
 )
 from PyQt5.QtGui import QPixmap, QPalette, QColor, QMovie
 from PyQt5.QtCore import Qt
+
+class ResultDialog(QDialog):
+    def __init__(self, message, gif_path, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("Result")
+        self.setFixedSize(400, 400)
