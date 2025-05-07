b7f9abd8e01979fec699b338b8823e23e971d2fa
Erica Guillermo <ericajhaneg@gmail.com>
2025-05-07 13:13:12 +0800

    Create quiz_maker.py
    importing libraries and gui

---
+++ quiz_maker.py
@@ -0,0 +1,8 @@
+import sys
+import random
+from PyQt5.QtWidgets import (
+    QApplication, QWidget, QLabel, QRadioButton, QPushButton,
+    QVBoxLayout, QMessageBox, QButtonGroup, QDialog
+)
+from PyQt5.QtGui import QPixmap, QPalette, QColor, QMovie
+from PyQt5.QtCore import Qt
