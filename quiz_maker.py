8035b449272c77191a5fdbfb143aa4d18bc9518d
Erica Guillermo <ericajhaneg@gmail.com>
2025-05-07 13:17:47 +0800

    Update quiz_maker.py
    adding widget for the result and using QMovie so i can add gif to the result window

--- quiz_maker.py
+++ quiz_maker.py
@@ -12,3 +12,11 @@ class ResultDialog(QDialog):
         super().__init__(parent)
         self.setWindowTitle("Result")
         self.setFixedSize(400, 400)
+        
+        layout = QVBoxLayout()
+
+        gif_label = QLabel()
+        movie = QMovie(gif_path)
+        gif_label.setMovie(movie)
+        movie.start()
+        layout.addWidget(gif_label, alignment=Qt.AlignCenter)
